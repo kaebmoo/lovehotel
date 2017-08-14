94e6022f88c6446b3f8ee300c0363eff5bdc99ab
Pornthep Nivatyakul <kaebmoo@yahoo.com>
2017-08-14 22:50:08 +0700

    update

--- openroom.sh
+++ openroom.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-mosquitto_pub -h localhost -t room1 -m "1" -u chang -P "chang"
-sleep 1m
-mosquitto_pub -h localhost -t room1 -m "0" -u chang -P "chang"
+if [ $# -eq 0 ]
+  then
+    echo "No arguments supplied"
+    exit 1
+fi
+readonly MIN=60
+readonly HOUR=3600
+((min=MIN*$1))
+((hour=HOUR*$1))
+date
+# echo $min
+# echo $hour
+echo $2 $min second
+mosquitto_pub -h register.local -t $2 -m "1" -u chang -P "chang"
+sleep $min
+date
+mosquitto_pub -h register.local -t $2 -m "0" -u chang -P "chang"
