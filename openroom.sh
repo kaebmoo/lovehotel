649743e60a3e1b2c63c29c8417edc0208131b0df
Pornthep Nivatyakul <kaebmoo@yahoo.com>
2017-08-15 07:09:03 +0700

    add log

--- openroom.sh
+++ openroom.sh
@@ -8,11 +8,11 @@ readonly MIN=60
 readonly HOUR=3600
 ((min=MIN*$1))
 ((hour=HOUR*$1))
-date
+echo "ON" `date`
 # echo $min
 # echo $hour
 echo $2 $min second
 mosquitto_pub -h register.local -t $2 -m "1" -u chang -P "chang"
 sleep $min
-date
+echo "OFF" `date`
 mosquitto_pub -h register.local -t $2 -m "0" -u chang -P "chang"
